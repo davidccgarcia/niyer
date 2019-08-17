ea8d94087f68e11126f48f2b0bee4256549c48e0
David García <ccristhiangarcia@gmail.com>
2019-08-17 11:21:43 -0500

    Test it load products page

--- tests/Feature/ProductsModuleTest.php
+++ tests/Feature/ProductsModuleTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Product;
 use App\User;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -36,5 +37,27 @@ class ProductsModuleTest extends TestCase
             'wholesale_unit_value' => '24.000',
             'unit_value' => '47.000'
         ]);
+    }
+
+    /**
+     * @test
+     */
+    public function it_load_products_page()
+    {
+        $this->withoutExceptionHandling();
+
+        $user = factory(User::class)->create();
+        $product1 = factory(Product::class)->create();
+        $product2 = factory(Product::class)->create();
+
+        $this->actingAs($user)
+            ->get(route('products'))
+            ->assertStatus(200)
+            ->assertSeeText('Products')
+            ->assertViewHas('products', function ($products) use ($product1, $product2) {
+                return $products->contains($product1) && $products->contains($product2);
+            })
+            ->assertSeeText($product1->name)
+            ->assertSeeText($product2->name);
     }
 }
