1cdcc7251a1e97d884d262d41484e2ee0f1a95cc
David García <ccristhiangarcia@gmail.com>
2019-08-21 14:09:05 -0500

    Add default value for ProductFactory photo field

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -9,6 +9,7 @@ $factory->define(Product::class, function (Faker $faker) {
     return [
         'name' => $faker->sentence,
         'description' => $faker->text,
+        'photo' => 'photos/wi57oGPEutvquPWRr67c8NlgjLS77BlcEYLqM97v.jpeg',
         'stock' => 12,
         'wholesale_unit_value' => '24.000',
         'unit_value' => '47.000'
