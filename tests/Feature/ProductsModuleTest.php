2f2fd49a82979fd5bbe0b3770776c2beb0c39ef5
David García <ccristhiangarcia@gmail.com>
2019-08-21 14:03:58 -0500

    Assert Database has photo field

--- tests/Feature/ProductsModuleTest.php
+++ tests/Feature/ProductsModuleTest.php
@@ -52,12 +52,13 @@ class ProductsModuleTest extends TestCase
             ->assertRedirect(route('products'));
 
         // Assert the file was stored...
-        Storage::disk('public')->assertExists('photos/' . $file->hashName());
+        $path = Storage::disk('public')->assertExists('photos/' . $file->hashName());
 
         $this->assertDatabaseHas('products', [
             'name' => 'Product 1',
             'description' => 'lorem ipsum dolor...',
             'stock' => 12,
+            'photo' => 'photos/' . $file->hashName(),
             'wholesale_unit_value' => '24.000',
             'unit_value' => '47.000'
         ]);
