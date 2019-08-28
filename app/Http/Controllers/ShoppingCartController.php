1b2a7377b4ee063c419a1b594ae7bb051ecb7e89
David García <ccristhiangarcia@gmail.com>
2019-08-28 14:54:27 -0500

    Get shopping cart and its associated products

--- app/Http/Controllers/ShoppingCartController.php
+++ app/Http/Controllers/ShoppingCartController.php
@@ -2,11 +2,31 @@
 
 namespace App\Http\Controllers;
 
-use App\ShoppingCart;
 use Illuminate\Http\Request;
+use App\{Product, ShoppingCart};
 
 class ShoppingCartController extends Controller
 {
+    /**
+     * Display a listing of the shopping carts.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $sessionID = session()->get('shopping_cart');
+        $shoppingCart = ShoppingCart::findOrCreate($sessionID);
+
+        $products = Product::with(['shoppingCarts' => function ($query) use ($sessionID) {
+            $query->where('shopping_cart_id', $sessionID);
+        }])->get();
+
+        return view('shopping_carts.index', [
+            'products' => $products,
+            'shoppingCart' => $shoppingCart,
+        ]);
+    }
+
     /**
      * Store a newly created resource in storage.
      *
