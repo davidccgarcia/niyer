e952972ab5232074635fc4d6a58834d2801973ce
David García <ccristhiangarcia@gmail.com>
2019-08-28 14:55:52 -0500

    Define relationship between products and shopping cart

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    /**
+     * The shopping carts that belongs to products.
+     */
+    public function shoppingCarts()
+    {
+        return $this->belongsToMany(ShoppingCart::class);
+    }
 }
