fd9b269b4b69c126374cf723a99c288ed10b0c60
David García <ccristhiangarcia@gmail.com>
2019-08-31 12:18:55 -0500

    Show order

--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -1,5 +1,36 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Order Created</h1>
+    <div class="container">
+        <h1>Order Created</h1>
+        <hr>
+        <div class="card">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-md-6">
+                        <strong>Name: </strong> {{ $order->receiver_name }}
+                    </div>
+                    <div class="col-md-6">
+                        <strong>Email: </strong> {{ $order->email }}
+                    </div>
+                </div>
+                <div class="row mt-3">
+                    <div class="col-md-6">
+                        <strong>Address: </strong> {{ $order->address }}
+                    </div>
+                    <div class="col-md-6">
+                        <strong>City: </strong> {{ $order->city }}
+                    </div>
+                </div>
+                <div class="row mt-3">
+                    <div class="col-md-6">
+                        <strong>Status: </strong> {{ $order->status }}
+                    </div>
+                    <div class="col-md-6">
+                        <strong>Guide Number: </strong> {{ $order->guide_number }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
